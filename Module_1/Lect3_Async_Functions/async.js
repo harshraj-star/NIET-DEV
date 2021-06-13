c67c569da368a24202e8a4580b2e88f230c73ae3
HARSH RAJ <hr45226@gmail.com>
2021-06-13 06:32:03 +0530

    Difference Between Sync And Async functions...

---
+++ Module_1/Lect3_Async_Functions/async.js
@@ -0,0 +1,10 @@
+let fs= require("fs");
+console.log("start");
+fs.readFile("./f1.txt" , cb);
+function cb(error ,data){
+    console.log(error);
+    console.log(data);
+
+    console.log(data + " ");
+}
+console.log("end");
